34043ee2e46b21048089013814755b59e1fabe56
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-01-23 13:06:34 +0530

    Should work now

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser);
-app.get("/api", (req, res) => {
-  const newValue = "helo";
-  res.json(newValue);
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
+  res.end();
 });
+
+app.use(bodyParser.json());
+app.use("/.netlify/functions/server", router);
 
 module.exports.handler = serverless(app);
