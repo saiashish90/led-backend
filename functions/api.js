b430b31545fc695a87e44589a5140a86df4123af
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-01-23 12:38:12 +0530

    Redirect

---
+++ functions/api.js
@@ -0,0 +1,10 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello World",
+      event: event,
+      context: context,
+    }),
+  };
+};
