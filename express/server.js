bf0c733612c5ad63d9a233fb9090142be460eb7a
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-01-23 14:32:40 +0530

    deprecated

--- express/server.js
+++ express/server.js
@@ -1,14 +1,27 @@
 "use strict";
+require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
-const app = express();
 const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const { PassThrough } = require("serverless-http/lib/request");
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE)),
+});
+const db = admin.firestore();
+const app = express();
 
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js!</h1>");
-  res.end();
+router.get("/", async (req, res) => {
+  if (Object.keys(req.query).length === 2) {
+    try {
+      const doc = await db.collection("leds").doc(req.query.user).get();
+      const leds = doc.data();
+      res.send(leds[req.query.led]);
+    } catch (e) {
+      res.send("invalid parameters");
+    }
+  } else res.send("invalid query");
 });
 
 app.use(bodyParser.json());
